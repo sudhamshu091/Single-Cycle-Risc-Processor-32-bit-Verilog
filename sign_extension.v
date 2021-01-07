3b29019ddfcf4ee5783d0753d6441427fa9a4466
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-08 00:14:05 +0530

    Create sign_extension.v

---
+++ sign_extension.v
@@ -0,0 +1,7 @@
+// sign-extend the 16-bit input to the 32_bit output
+module sign_extension (sign_in, sign_out);
+	input [15:0] sign_in;
+	output [31:0] sign_out;
+	assign sign_out[15:0]=sign_in[15:0];
+	assign sign_out[31:16]=sign_in[15]?16'b1111_1111_1111_1111:16'b0;
+endmodule
